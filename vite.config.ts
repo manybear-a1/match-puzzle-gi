bd476c1c1a49362080af0debe14247f78d4f10a8
many bear <98581612+6666dustry@users.noreply.github.com>
2025-10-07 20:32:51 +0900

    Work around

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/match-puzzle-gi/'
+  base: '/match-puzzle-gi/',
+  assetsInclude: ['**/*.md'],
 });
